f2e46ddda3c6c13593fb9c98c8df6e96ec1f5b1d
Idan91 <idan.ram91@gmail.com>
2019-11-13 13:12:08 +0200

    added social media icons

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const portfolios = document.querySelector("#folios");
 const boxes = document.querySelector(".box-container");
 const devBox = document.querySelector(".dev-box");
 const photoBox = document.querySelector(".photo-box");
+const linkedIn = document.querySelector("#linkedIn");
+const insta = document.querySelector("#insta");
 const footer = document.querySelector("footer");
 
 const timeline = new TimelineMax();
@@ -31,9 +33,17 @@ timeline
     "-=1.8"
   )
   .fromTo(
+    linkedIn,
+    2,
+    { x: "-5000%" },
+    { x: "0%", ease: Power2.easeOut },
+    "-=1.5"
+  )
+  .fromTo(insta, 2, { x: "5000%" }, { x: "0%", ease: Power2.easeOut }, "-=2")
+  .fromTo(
     footer,
     1.5,
     { y: "3000%" },
     { y: "0%", ease: Power2.easeOut },
-    "-=1.3"
+    "-=1.5"
   );
