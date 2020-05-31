894d109e8c346a54ab94db4d4fa396fa61ad8e8a
Idan91 <idan.ram91@gmail.com>
2020-06-01 00:32:32 +0300

    Katia

---
+++ katia2020/precache-manifest.738db293d3fa686cb05bca76768ae6f3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "039bbf0c972d480ff46caa3857338c99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72524ed0f560a28d6946",
+    "url": "/static/css/main.f8f16cd3.chunk.css"
+  },
+  {
+    "revision": "9999f5da838c73f2bd68",
+    "url": "/static/js/2.8f27e98c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f27e98c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72524ed0f560a28d6946",
+    "url": "/static/js/main.59f51edd.chunk.js"
+  },
+  {
+    "revision": "13aed49193680ac60320",
+    "url": "/static/js/runtime-main.ce424d72.js"
+  },
+  {
+    "revision": "d1f6a7c95a944c0b6f749ac48d6f8096",
+    "url": "/static/media/3480494_2-01.d1f6a7c9.svg"
+  },
+  {
+    "revision": "64a62eed98cfbe11d47be159d26fa343",
+    "url": "/static/media/IMG_2798.64a62eed.jpeg"
+  },
+  {
+    "revision": "5dd02efd0004044fb6560d2706746e77",
+    "url": "/static/media/IMG_3400.5dd02efd.jpeg"
+  },
+  {
+    "revision": "3993542bfc08e26c5ce49ef7eac7c686",
+    "url": "/static/media/IMG_3704.3993542b.jpeg"
+  },
+  {
+    "revision": "2f8beeb69784d2ca450a5fc7ba7d83be",
+    "url": "/static/media/IMG_3746.2f8beeb6.jpeg"
+  },
+  {
+    "revision": "d41e5e4febd1ea64f5ef59ae52f5d75b",
+    "url": "/static/media/IMG_5992.d41e5e4f.jpeg"
+  }
+]);
