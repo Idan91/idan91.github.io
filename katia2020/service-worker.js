d7934531b709552bf422bb6b6fd1e2eeb84f4cce
Idan91 <idan.ram91@gmail.com>
2020-06-01 00:39:13 +0300

    Katia - 2nd try

--- katia2020/service-worker.js
+++ katia2020/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.738db293d3fa686cb05bca76768ae6f3.js"
+  "./precache-manifest.b375b01ecbf215e210a102231a5285b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
